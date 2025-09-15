a4a9abe0085edeb53a105b4f77da1824366bbf60
dovaogedot <dovaogedot@gmail.com>
2025-09-15 21:33:33 +0300

    Added drag area

--- src/components/SolarSystem.tsx
+++ src/components/SolarSystem.tsx
@@ -16,39 +16,6 @@ export const SolarSystem = (props: {
     const [zoom, setZoom] = useState(192)
     const dragging = useRef(false)
 
-    useEffect(() => {
-        const handleWheel = (e: WheelEvent) => {
-            setZoom(x => x * (e.deltaY < 0 ? 1.1 : 0.9))
-        }
-
-        const handleMouseDown = (e: MouseEvent) => {
-            if (e.button == 0) dragging.current = true
-        }
-
-        const handleMouseUp = (e: MouseEvent) => {
-            if (e.button == 0) dragging.current = false
-        }
-
-        const handleMouseMove = (e: MouseEvent) => {
-            if (dragging.current == true) {
-                setSunX(x => x + e.movementX)
-                setSunY(y => y + e.movementY)
-            }
-        }
-
-        window.addEventListener("wheel", handleWheel, { passive: true })
-        window.addEventListener("mousedown", handleMouseDown)
-        window.addEventListener("mouseup", handleMouseUp)
-        window.addEventListener("mousemove", handleMouseMove)
-
-        return () => {
-            window.removeEventListener("wheel", handleWheel)
-            window.removeEventListener("mousedown", handleMouseDown)
-            window.removeEventListener("mouseup", handleMouseUp)
-            window.removeEventListener("mousemove", handleMouseMove)
-        }
-    }, [])
-
     const [sunX, setSunX] = useState(window.innerWidth / 2)
     const [sunY, setSunY] = useState(window.innerHeight / 2)
 
@@ -59,22 +26,39 @@ export const SolarSystem = (props: {
             <div className="options">
                 <Slider label={"Time"} min={0} max={1} step={0.01} onChange={setTime} />
             </div>
-            <div className="sun" style={{
-                "--radius": sizeFunc(sun.radius) + "px",
-                "--color": sun.color,
-                "--time": time,
-                "left": sunX + "px",
-                "top": sunY + "px"
-            } as React.CSSProperties}>
-                <div className="disk">
-                    <h5>Sun</h5>
+            <div className="drag-area"
+                onMouseDown={e => {
+                    if (e.button == 0) dragging.current = true
+                }}
+                onMouseUp={e => {
+                    if (e.button == 0) dragging.current = false
+                }}
+                onMouseMove={e => {
+                    if (dragging.current == true) {
+                        setSunX(x => x + e.movementX)
+                        setSunY(y => y + e.movementY)
+                    }
+                }}
+                onWheel={e => {
+                    setZoom(x => x * (e.deltaY < 0 ? 1.1 : 0.9))
+                }}>
+                <div className="sun" style={{
+                    "--radius": sizeFunc(sun.radius) + "px",
+                    "--color": sun.color,
+                    "--time": time,
+                    "left": sunX + "px",
+                    "top": sunY + "px"
+                } as React.CSSProperties}>
+                    <div className="disk">
+                        <h5>Sun</h5>
 
-                    {sun.planets.map(planet =>
-                        <Planet key={planet.name}
-                            data={planet}
-                            sizeFunc={sizeFunc}
-                            distFunc={distFunc} />)}
+                        {sun.planets.map(planet =>
+                            <Planet key={planet.name}
+                                data={planet}
+                                sizeFunc={sizeFunc}
+                                distFunc={distFunc} />)}
 
+                    </div>
                 </div>
             </div>
         </div>
